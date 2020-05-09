03ad19937b1e7d96fca4959ab4376d96133b9548
gangtori <kimkwangsik@gmail.com>
2020-05-09 11:31:54 +0900

    0.41.19

---
+++ 200509/fuction.py
@@ -0,0 +1,15 @@
+# def  add5(number):
+#     result = number + 5
+#     return result
+#
+# # print(add5(3))
+# def mult6(number):
+#     result = number * 6
+#     return result
+#
+# print( mult6(add5(8)))
+def add(a,b):
+    return a+b
+    print("나는 전설이다.")
+
+print(add(3,9))
